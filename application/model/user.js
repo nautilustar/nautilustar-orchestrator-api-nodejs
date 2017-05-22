54ff86b84d96685dc75b117714f9b0c50e0a7476
charleston anjos <charleston.anjos@gmail.com>
2017-05-22 20:45:25 -0300

    scheme fields

--- application/model/user.js
+++ application/model/user.js
@@ -5,7 +5,27 @@ var mongoose = require('mongoose'),
  * User Schema 
  */
 var UserSchema = new Schema({
-    
+    user_name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);  
